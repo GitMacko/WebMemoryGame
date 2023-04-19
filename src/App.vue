91e34d5fe1b027d5a7fdc8a95c88c9f9f9cf42b6
GitMacko <GitMacko@example.com>
2023-04-19 23:57:06 +0200

    First commit, game board and Card component

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<script>
+import { ref } from 'vue'
+import Card from './components/Card.vue'
+
+export default {
+  name: 'WebMemoryGame',
+  components:{
+    Card
+  },
+
+  setup() {
+    const TablicaKart = ref([])
+    for(let i=0; i<4; i++){
+      TablicaKart.value.push({
+        value: i,
+        visible: false,
+        position:i
+    })
+      
+  }
+  const odwrocKarte = (payload) =>{
+  TablicaKart.value[payload.position].visible = true
+  }
+
+    return {
+      TablicaKart,
+      odwrocKarte
+    }
+  },
+
+}
+
+
+
+
+
+</script>
+
+<template>
+ <h1>MemorizeIT</h1>
+ <section class = "plansza">
+
+  <Card v-for="(karta, index) in TablicaKart"
+   :key="'card-${index}'" 
+   :value="karta.value" 
+   @wybierz-karte="odwrocKarte" 
+   :position="karta.position"
+   :visible="karta.visible"
+   />
+
+ </section>
+
+</template>
+
+
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+}
+
+.plansza{
+  display:grid;
+  grid-template-columns:100px 100px ;
+  grid-template-rows: 100px 100px;
+  grid-column-gap: 30px;
+  grid-row-gap: 30px;
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,65 @@
+<script>
+export default {
+  props: {
+    position:{
+        type:Number,
+        required:true
+    },
+    value: {
+        type: Number,
+        required: true
+    },
+    visible:{
+        type: Boolean,
+        default: false
+  }
+  },
+  setup(props,context){
+
+
+
+const wybierzKarte = () => 
+context.emit('wybierz-karte',{
+    position: props.position
+})
+return{
+    wybierzKarte
+}
+  }
+}
+</script>
+
+
+<template>
+    <div class="karta" @click="wybierzKarte">
+        <div v-if = "visible" class="strona-karty przod ">
+            {{ value }}
+        </div>
+        <div v-else class="strona-karty tyl">
+            Tył
+        </div>
+    </div>
+</template>
+<style>
+
+.karta{
+    border: 5px solid black;
+    position:relative;
+}
+
+.strona-karty{
+    width: 100%;
+    height: 100%;
+    position:absolute;
+}
+
+
+
+.strona-karty.przod{
+    background-color: blue;
+}
+.strona-karty.tyl{
+    background-color: red;
+}
+
+</style>
